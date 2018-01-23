cf94e0bf22ed82d9abc44eca9499eb14f2a5df4b
SongFeng <SongFeng@email.com>
2018-01-23 22:21:25 +0800

    Second Commit

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by Administrator on 2018\1\23 0023.
+ */
+
+public class Province{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
